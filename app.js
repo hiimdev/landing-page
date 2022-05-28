e67f2b4b1b3df7639714572723dab78f51780850
huycan <vohuy21032001@gmail.com>
2022-05-28 22:52:39 +0700

    edit carousel customers and section subcribe

--- app.js
+++ app.js
@@ -41,3 +41,49 @@ modal.addEventListener('click', hideBuyTickets);
 modalContainer.addEventListener('click', function (event) {
     event.stopPropagation();
 });
+
+// carousel customers
+$('.customer-list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    // adaptiveHeight: true,
+    prevArrow: `<button type='button' class='slick-prev pull-left'><img src="./assets/imgs/Icon/arrow-left.svg"/></button>`,
+    nextArrow: `<button type='button' class='slick-next pull-right'><img src="./assets/imgs/Icon/arrow-left.svg"/></button>`,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+            },
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            },
+        },
+        {
+            breakpoint: 1023,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+            },
+        },
+    ],
+});
